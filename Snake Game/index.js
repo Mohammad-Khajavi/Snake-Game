1c0831b62a4f2ab53121e5a3a53ad7066fd558cd
Mohammad-Khajavi <151404726+Mohammad-Khajavi@users.noreply.github.com>
2024-10-28 22:37:36 -0400

    java script update
    added the logic for the snake movement

--- Snake Game/index.js
+++ Snake Game/index.js
@@ -53,7 +53,8 @@ function nextTick() {
 	}
 };
 function clearBoard() {
-	
+	ctx.fillStyle = boardBackGround;
+	ctx.fillRect(0, 0, gameWidth, gameHeight);
 };
 function createFood() {
 	function randomFood(min, max){
@@ -68,9 +69,67 @@ function drawFood() {
 	ctx.fillStyle = foodColor;
 	ctx.fillRect(foodX, foodY, unitSize, unitSize);
 };
-function moveSnake() {};
-function drawSnake() {};
-function changeDirection() {};
+function moveSnake() {
+	const head = {x: snake[0].x + xVelocity, 
+		y: snake[0].y + yVelocity};
+
+		snake.unshift(head);
+
+		if(snake[0].x == foodX && sanke[0].y == foodY){
+			score++;
+			scoreText.textContent = score;
+			createFood();
+		}else{
+			snake.pop
+		}
+};
+function drawSnake() {
+	ctx.fillStyle = snakeColor;
+	ctx.strokeStyle = snakeBoarder;
+	snake.forEach(snakePart =>{
+		ctx.fillRect(snakePart.x, snakePart.y, unitSize);
+		ctx.strokeRect(snakePart.x, snakePart.y, unitSize);
+
+	})
+};
+function changeDirection(event) {
+	const keyPressed = event.keyCode;
+	const LEFT = 37;
+	const UP = 38;
+	const RIGHT = 39;
+	const DOWN = 40;
+
+	const goingUp = (yVelocity == -unitSize);
+	const goingDown = (yVelocity == unitSize);
+	const goingRight = (xVelocity == unitSize);
+	const goingLeft = (xVelocity == -unitSize);
+
+	switch(true){
+		case(keyPressed == LEFT && !goingRight):
+			xVelocity = -unitSize;
+			yVelocity = 0;
+			break;
+		
+		case(keyPressed == Up && !goingDown):
+			xVelocity = 0;
+			yVelocity = -unitSize;
+			break;
+		
+		case(keyPressed == RIGHT && !goingLeft):
+			xVelocity = unitSize;
+			yVelocity = 0;
+			break;
+
+		case(keyPressed == DOWN && !goingUp):
+			xVelocity = 0;
+			yVelocity = unitSize;
+			break;
+	}
+
+
+
+
+};
 function checkGameOver() {};
 function displayGameOver() {};
 function resetGame() {}; 
